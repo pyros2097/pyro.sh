4e047a283561961d67e6be5ff14e4fc19a60ab36
pyros2097 <pyros2097@gmail.com>
2021-02-12 11:53:08 +0530

    new stuff

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 IP=192.168.0.104
 NODE_ENV=production
-WATCH=false
-npm run css
 npm run website
 npm run timer
-rsync -aP --delete assets.pyro.sh pi@$IP:/home/pi
 rsync -aP --delete pyro.sh pi@$IP:/home/pi
 rsync -aP --delete timer.pyro.sh pi@$IP:/home/pi
 

c9db961440e64b106bfb1bb22ca6e711529b4b14
pyros2097 <pyros2097@gmail.com>
2021-02-15 21:28:15 +0530

    improv

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-IP=192.168.0.104
-NODE_ENV=production
-npm run website
-npm run timer
-rsync -aP --delete pyro.sh pi@$IP:/home/pi
-rsync -aP --delete timer.pyro.sh pi@$IP:/home/pi
-
-# systemctl reload caddy

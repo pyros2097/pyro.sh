4af8c823b6fbef933dc7a8af886796b80c30d492
pyros2097 <pyros2097@gmail.com>
2021-02-27 22:04:43 +0530

    complete mobile view

--- bible.pyro.sh/sw.js
+++ bible.pyro.sh/sw.js
@@ -1,5 +1,5 @@
-const urlsToCache = ['/', '/index.html', '/assets/alpine.js', '/assets/logo.png', '/assets/styles.css'];
-const version = 'bible-v0.1.0'
+const urlsToCache = ['/', '/index.html', '/assets/alpine.js', '/assets/logo.png', '/assets/styles.css'];  
+const version = 'bible-v0.1.3'
 
 self.addEventListener('install', function (event) {
   self.skipWaiting();

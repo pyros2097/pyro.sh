1d2ab373def04fd29264bc8e93174814e8cfc9f0
pyros2097 <pyros2097@gmail.com>
2021-03-01 12:15:04 +0530

    fix bible app

--- bible.pyro.sh/sw.js
+++ bible.pyro.sh/sw.js
@@ -1,5 +1,5 @@
 const urlsToCache = ['/', '/index.html', '/assets/alpine.js', '/assets/logo.png', '/assets/styles.css'];  
-const version = 'bible-v0.1.3';
+const version = 'bible-v0.1.4';
 
 self.addEventListener('install', function (event) {
   self.skipWaiting();

13cdf9cb805b404bf45379bfc086fb58331bb65f
Peter John <peter.john@johnlewis.co.uk>
2021-07-03 15:46:46 +0530

    improve ref

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,41 +22,45 @@ export const head = ({ config }) => {
 
 const slides = [
   {
-    title: "Website",
-    subTitle: "This website is hosted on a Raspberry Pi 4 with 4GB RAM. The host IP is 106.51.19.44.",
-    description: "Makes it easier for me to deploy stuff to immediately. But at the consequence of higher latency for people living outside India.",
-    imgSrc: "/assets/images/pi4.jpg",
-    imgWidth: "300",
+    title: 'Website',
+    subTitle: 'This website is hosted on a Raspberry Pi 4 with 4GB RAM. The host IP is 106.51.19.44.',
+    description: 'Makes it easier for me to deploy stuff to immediately. But at the consequence of higher latency for people living outside India.',
+    imgSrc: '/assets/images/pi4.jpg',
+    imgWidth: '300',
   },
   {
-    title: "rust-embed",
-    subTitle: "A rust macro which loads files into the rust binary at compile time during release and loads the file from the fs during dev.",
-    description: "You can use this to embed your css, js and images into a single executable which can be deployed to your servers. Also it makes it easy to build a very small docker image for you to deploy.",
-    imgSrc: "/assets/images/rust-embed.png",
-    imgWidth: "100%",
+    title: 'rust-embed',
+    subTitle: 'A rust macro which loads files into the rust binary at compile time during release and loads the file from the fs during dev.',
+    description:
+      'You can use this to embed your css, js and images into a single executable which can be deployed to your servers. Also it makes it easy to build a very small docker image for you to deploy.',
+    imgSrc: '/assets/images/rust-embed.png',
+    imgWidth: '100%',
   },
   {
-    title: "wapp",
-    subTitle: "wapp is a framework to build isomorphic web apps in golang.",
-    description: "It uses a declarative syntax using funcs that allows creating and dealing with HTML elements only by using Go, and without writing any HTML markup. The syntax is inspired by react and its awesome hooks and functional component features. It is highly opioninated and integrates very well with tailwind css for now.",
-    imgSrc: "/assets/images/rust-embed.png",
-    imgWidth: "100%",
+    title: 'wapp',
+    subTitle: 'wapp is a framework to build isomorphic web apps in golang.',
+    description:
+      'It uses a declarative syntax using funcs that allows creating and dealing with HTML elements only by using Go, and without writing any HTML markup. The syntax is inspired by react and its awesome hooks and functional component features. It is highly opioninated and integrates very well with tailwind css for now.',
+    imgSrc: '/assets/images/rust-embed.png',
+    imgWidth: '100%',
   },
   {
-    title: "Pine",
-    subTitle: "A programming language with a syntax largely inspired by nim but with the simplicity of go. It has jsx support in built by default.",
-    description: "first class functions clean syntax basics: int, float, bool, byte, enum references: string, array, map, nil, error functions: extern, proc, method, test conditions: if, elif, else, match, break, continue loops: for inbuilt: echo, assert",
-    imgSrc: "/assets/images/pine.png",
-    imgWidth: "50%",
+    title: 'Pine',
+    subTitle: 'A programming language with a syntax largely inspired by nim but with the simplicity of go. It has jsx support in built by default.',
+    description:
+      'first class functions clean syntax basics: int, float, bool, byte, enum references: string, array, map, nil, error functions: extern, proc, method, test conditions: if, elif, else, match, break, continue loops: for inbuilt: echo, assert',
+    imgSrc: '/assets/images/pine.png',
+    imgWidth: '50%',
   },
   {
-    title: "Gdx Studio",
-    subTitle: "GdxStudio is used for creating awesome games using libGdx.",
-    description: "It has all the features of libgdx built-in so you can easily,start creating games with it. Automatic Asset Loading including Atlas, TextureRegions, BitmapFonts, Music, Sound. Tools like Font Editor, Particle Editor, Texture Packer, SceneEditor, MapEditor, ActorEditor, ImagingTools are already built into it.",
-    imgSrc: "/assets/images/gdx-studio.png",
-    imgWidth: "100%",
-  }
-]
+    title: 'Gdx Studio',
+    subTitle: 'GdxStudio is used for creating awesome games using libGdx.',
+    description:
+      'It has all the features of libgdx built-in so you can easily,start creating games with it. Automatic Asset Loading including Atlas, TextureRegions, BitmapFonts, Music, Sound. Tools like Font Editor, Particle Editor, Texture Packer, SceneEditor, MapEditor, ActorEditor, ImagingTools are already built into it.',
+    imgSrc: '/assets/images/gdx-studio.png',
+    imgWidth: '100%',
+  },
+];
 
 export const body = () => {
   const [item, setItem] = useState(0);
@@ -68,14 +72,11 @@ export const body = () => {
         <div class="flex flex-row flex-1 items-center max-w-5xl text-lg font-source p-2 mt-4 sm:p-4">
           <div>
             <div>
-              Hi there, I'm <strong>Peter John</strong>, a fullstack developer from Bangalore, India. I love writing code
-              and I am lucky enough to do
-              this as my job. I currently work for Equal Experts. I have a strong passion for golang but I also work with
-              react and nodejs.
+              Hi there, I'm <strong>Peter John</strong>, a fullstack developer from Bangalore, India. I love writing code and I am lucky enough to do this as my
+              job. I currently work for Equal Experts. I have a strong passion for golang but I also work with react and nodejs.
               <p>
-                I like to work on open source and hobby projects. Over the course of 8 years I've accumlated a lot of useful
-                projects used by many
-                people around the world.
+                I like to work on open source and hobby projects. Over the course of 8 years I've accumlated a lot of useful projects used by many people around
+                the world.
               </p>
             </div>
             <article>
@@ -83,13 +84,18 @@ export const body = () => {
                 <ul class="flex flex-row mb-4">
                   ${slides.map((_, index) => {
                     const selected = index === item;
-                    return (
-                      html`
-                        <li @click=${() => setItem(index)} class="px-3 py-1 cursor-pointer select-none ${classMap({ 'bg-black': selected, 'text-white': selected, 'hover:bg-gray-300': index !== item})}">
-                        ${index+1}
-                        </li>
-                      `
-                    )
+                    return html`
+                      <li
+                        @click=${() => setItem(index)}
+                        class="px-3 py-1 cursor-pointer select-none ${classMap({
+                          'bg-black': selected,
+                          'text-white': selected,
+                          'hover:bg-gray-300': index !== item,
+                        })}"
+                      >
+                        ${index + 1}
+                      </li>
+                    `;
                   })}
                 </ul>
                 <div class="bg-slider mb-8 rounded-sm overflow-hidden">
@@ -99,7 +105,8 @@ export const body = () => {
                       subTitle=${currentSlide.subTitle}
                       description=${currentSlide.description}
                       imgSrc=${currentSlide.imgSrc}
-                      imgWidth=${currentSlide.imgWidth}>
+                      imgWidth=${currentSlide.imgWidth}
+                    >
                     </app-slide>
                   </div>
                 </div>
@@ -131,12 +138,9 @@ export const body = () => {
                   <div class="text-2xl font-bold mt-6"><i class="icon-chat"></i> Contact</div>
                   <div class="mt-6">You can contact me through any of these methods</div>
                   <div class="mt-6 text-lg">
-                    <div>Email: <a class="text-md text-newblue ml-1"
-                        href="mailto:pyros2097@gmail.com">pyros2097@gmail.com</a></div>
-                    <div class="mt-2">Github: <a class="text-md text-newblue ml-1"
-                        href="https://github.com/pyros2097">pyros2097</a></div>
-                    <div class="mt-2">LinkedIn: <a class="text-md text-newblue ml-1"
-                        href="https://www.linkedin.com/in/pyros2097">pyros2097</a></div>
+                    <div>Email: <a class="text-md text-newblue ml-1" href="mailto:pyros2097@gmail.com">pyros2097@gmail.com</a></div>
+                    <div class="mt-2">Github: <a class="text-md text-newblue ml-1" href="https://github.com/pyros2097">pyros2097</a></div>
+                    <div class="mt-2">LinkedIn: <a class="text-md text-newblue ml-1" href="https://www.linkedin.com/in/pyros2097">pyros2097</a></div>
                   </div>
                 </div>
               </div>
--- src/pages/ref.js
+++ src/pages/ref.js
@@ -26,9 +26,9 @@ export const body = () => {
         <div class="flex flex-row flex-1 items-center max-w-5xl text-lg font-source p-4 mt-4">
           <div>
             <div>
-              Programming is my passion. I've been coding from a very young age and still do so. I have created and contributed to many open source
-              projects. My main interest lies in is backend, frontend, and devops. I build frameworks and automate common tasks to make it easier to
-              develop applications. I do occasionally work on creating 2d games.
+              Programming is my passion. I've been coding from a very young age and still do so. I have created and contributed to many open source projects. My
+              main interest lies in frontend, backend, and devops. I build frameworks and automate common tasks to make it easier to develop applications. I do
+              occasionally work on creating 2d games.
             </div>
             <div class="flex flex-1 flex-col">
               <div class="text-2xl font-bold mt-6">Experience</div>
@@ -42,27 +42,41 @@ export const body = () => {
                   These are the client projects I worked on,
                   <ul class="mt-2 ml-6 sm:ml-10 list-disc list-outside">
                     <li>
+                      John Lewis
+                      <ul class="mt-2 ml-6 sm:ml-10 list-disc list-outside">
+                        <li>Built the new Product Description Page (PDP) for Simple Carpets using JLDP blueprint template (nextjs) in team-home-interiors</li>
+                        <li>Helped build, improve, and run AB experiments in the customer services and my orders pages in team-selfserve</li>
+                      </ul>
+                    </li>
+                    <li>
                       iOWNA
                       <ul class="mt-2 ml-6 sm:ml-10 list-disc list-outside">
                         <li>Built a content editor for curators to create content on our platform</li>
-                        <li>Built the iOWNA crossplatform app in react-native that works in android, ios, web. One codebase.</li>
+                        <li>Built the iOWNA crossplatform app in react-native that works in android, ios, web. One codebase</li>
+                        <li>Implemented admin specific functionalities in the web app</li>
+                        <li>Implemented an in-app OTA update method using expo and semantic versionin</li>
+                        <li>Built the iOWNA marketing website</li>
+                        <li>Built the Health Questionnaire clinician/patient feedback form feature with push notifications</li>
                       </ul>
                     </li>
                     <li>
                       Zeta
                       <ul class="mt-2 ml-6 sm:ml-10 list-disc list-outside">
-                        <li>Used k8s operator framework to automate creating acls in sandbox using custom resources</li>
-                        <li>Created a Groovy Script to automate creating acls in sandbox authorization framework</li>
-                        <li>Added multiple formats(pdf,html,xlsx) to download reports in Report Center</li>
+                        <li>Added multiple download formats (pdf,html,xlsx) for reports in the Reporting Center</li>
+                        <li>Created a Groovy Script to automate creating ACL's in sandbox authorization framework</li>
                         <li>Integrated Camunda Workflow Engine into Zeta services in Operations Center</li>
+                        <li>Built a few workflows on Camunda and integrated it with some internal services</li>
+                        <li>Built a custom k8s CRD using the operator framework to automate creating ACL's in sandbox using custom resources</li>
                       </ul>
                     </li>
                     <li>
                       Lifebox
                       <ul class="mt-2 ml-6 sm:ml-10 list-disc list-outside">
-                        <li>Added search functionaLity to the hospital procedure codes</li>
-                        <li>Converted the Health Questionnaire from json to go structs/funcs making it easier to make changes to it</li>
+                        <li>Added search functionality to the hospital procedure codes</li>
+                        <li>Converted the Health Questionnaire from json to native go code making it easier to make changes to it</li>
                         <li>Integrated with SAP using HL7 message format to create patients, episodes, schedule/cancel procedures</li>
+                        <li>Built the Patient Document repository for uploading, downloading patient files</li>
+                        <li>Built the patients online reports view using the data from SAP</li>
                       </ul>
                     </li>
                   </ul>
@@ -72,9 +86,9 @@ export const body = () => {
                 <div class="text-xl font-bold mt-6">Numberz</div>
                 <div class="text-lg font-semibold mt-1">Full Stack Developer, Sept 2016 - Oct 2018</div>
                 <p class="text-black font-source mt-2">
-                  Numberz integrates banking with your day-2-day business work-flows freeing up a lot of effort, time and heart-burn, to help your
-                  business grow! You’ll never have to spend time syncing information between different systems. Save and record receipts &amp; spends
-                  with a single tap, automatically categorise transactions for book-keeping and easily share information with your accountant.
+                  Numberz integrates banking with your day-2-day business work-flows freeing up a lot of effort, time and heart-burn, to help your business
+                  grow! You’ll never have to spend time syncing information between different systems. Save and record receipts &amp; spends with a single tap,
+                  automatically categorise transactions for book-keeping and easily share information with your accountant.
                 </p>
                 <ul class="mt-2 ml-6 sm:ml-10 list-disc list-outside">
                   <li>Implemented new features and fixed bugz on the numberz android app</li>
@@ -84,15 +98,13 @@ export const body = () => {
                   <li>Started tech talks on Thursdays to discuss cutting edge technologies</li>
                   <li>Architected and built the Notification Service using rabbitmq and nodejs</li>
                   <li>Built the Multi-User and Multi-Company Features</li>
-                  <li>
-                    Implemented and lead a team on integration with the GSTN System so that customers could file their GST Taxes within our product
-                  </li>
+                  <li>Implemented and lead a team on integration with the GSTN System so that customers could file their GST Taxes within our product</li>
                   <li>Architected and built the numberz Identity Management Service(IMS)</li>
                   <li>Architected our new product called the Numberz Accounts Receivable (AR)</li>
                   <li>Laid down the architecture for the frontend and backend</li>
                   <li>
-                    Built these core features for our new AR Product - Auto Reminders, Manual Reminders, User Management, Cash Discounting, CFO
-                    Dashboard Metrics
+                    Built these core features for our new AR Product - Auto Reminders, Manual Reminders, User Management, Cash Discounting, CFO Dashboard
+                    Metrics
                   </li>
                 </ul>
               </div>
@@ -100,10 +112,9 @@ export const body = () => {
                 <div class="text-xl font-bold mt-6">Playlyfe</div>
                 <div class="text-lg font-semibold mt-1">Full Stack Developer, Apr 2014 - Sept 2016</div>
                 <p class="text-black font-source mt-2">
-                  Playlyfe is an online Gamification Platform which empowers anyone to design and implement a gamified system.This platform allows a
-                  normal user to convert a gamified system's design into a fully functional web-application complete with teams, real-time
-                  notifications, leaderboards and many more features. Made Critical decisions on what tech stack to switch to for our gamification
-                  product Catalyst.
+                  Playlyfe is an online Gamification Platform which empowers anyone to design and implement a gamified system.This platform allows a normal user
+                  to convert a gamified system's design into a fully functional web-application complete with teams, real-time notifications, leaderboards and
+                  many more features. Made Critical decisions on what tech stack to switch to for our gamification product Catalyst.
                 </p>
                 <ul class="mt-2 ml-6 sm:ml-10 list-disc list-outside">
                   <li>Built the Playlyfe Hybrid Mobile app using Phonegap for Android and iOS</li>
